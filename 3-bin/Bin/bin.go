0089acf81f6dfa5fe016e884592e92f6e4af4999
Vladimir Berestnev <berestnew@yandex.ru>
2026-08-17 16:00:59 +0300

    3-struct-fix2

---
+++ 3-bin/Bin/bin.go
@@ -0,0 +1,29 @@
+package bin
+
+import "time"
+
+type Bin struct {
+	id        string
+	private   bool
+	createdAt time.Time
+	name      string
+}
+
+func newBin(id string, private bool, name string) *Bin {
+	return &Bin{
+		id:        id,
+		private:   private,
+		createdAt: time.Now(),
+		name:      name,
+	}
+}
+
+type BinList struct {
+	Bins []Bin
+}
+
+func newBinList() *BinList {
+	return &BinList{
+		Bins: make([]Bin, 0),
+	}
+}
--- 3-bin/main.go
+++ 3-bin/main.go
@@ -1,33 +1,5 @@
 package main
 
-import "time"
-
-type Bin struct {
-	id        string
-	private   bool
-	createdAt time.Time
-	name      string
-}
-
-func newBin(id string, private bool, name string) *Bin {
-	return &Bin{
-		id:        id,
-		private:   private,
-		createdAt: time.Now(),
-		name:      name,
-	}
-}
-
-type BinList struct {
-	Bins []Bin
-}
-
-func newBinList() *BinList {
-	return &BinList{
-		Bins: make([]Bin, 5),
-	}
-}
-
 func main() {
 
 }

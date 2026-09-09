4fb8bc376acb1c7ac5e8770410c43f3710d81350
Vladimir Berestnev <berestnew@yandex.ru>
2026-09-09 12:33:17 +0300

    3-struct-os-fix2

--- 3-bin/storage/storage.go
+++ 3-bin/storage/storage.go
@@ -7,38 +7,39 @@ import (
 	"os"
 )
 
-func SaveBins(binList bin.BinList, filename string) {
+func SaveBins(binList bin.BinList, filename string) error {
 	data, err := json.Marshal(binList)
 	if err != nil {
 		fmt.Println("Ошибка сериализации json")
-		return
+		return err
 	}
 	file, errCreate := os.Create(filename)
 	if errCreate != nil {
 		fmt.Println("Ошибка создания файла")
-		return
+		return err
 	}
 	defer file.Close()
 
 	_, errWrite := file.WriteString(string(data))
 	if errWrite != nil {
 		fmt.Println("Ошибка записи в файл")
-		return
+		return err
 	}
 	fmt.Println("Запись успешна")
+	return nil
 }
 
 func ReadBins(filename string) (bin.BinList, error) {
-	bins := bin.NewBinList()
+	var bins bin.BinList
 	data, err := os.ReadFile(filename)
 	if err != nil {
 		fmt.Println("Ошибка чтения файла")
-		return *bins, err
+		return bins, err
 	}
 	err = json.Unmarshal(data, &bins)
 	if err != nil {
 		fmt.Println("Ошибка парсинга JSON")
-		return *bins, err
+		return bins, err
 	}
-	return *bins, nil
+	return bins, nil
 }
